38fff98e19d306d0dc675005cef0f7a19b588589
Lucas Matheus <105503258+LmarDark@users.noreply.github.com>
2023-06-18 19:01:22 -0300

    Update gabiru.php

--- gabiru.php
+++ gabiru.php
@@ -1,7 +1,7 @@
 <?php 
 
 $name = $_POST["name"];
-$email =  $_POST["message"];
-$password = $_POST["priority"];
+$email =  $_POST["email"];
+$password = $_POST["password"];
 
 var_dump($name, $email, $password);
