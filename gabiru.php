22ee3d0b7228892ba85a9c1a6975df963aea209a
Lucas Matheus <105503258+LmarDark@users.noreply.github.com>
2023-06-19 12:12:16 -0400

    Update gabiru.php

--- gabiru.php
+++ gabiru.php
@@ -5,3 +5,15 @@ $email =  $_POST["email"];
 $password = $_POST["password"];
 
 var_dump($name, $email, $password);
+
+/* $servidor = localhost;
+$usuario = root;
+$senha = geralmente_nada/vazio;
+$db = não_me_recordo;
+
+$conexao = mysql_connect($servidor, $usuario, $senha, $db);
+
+if(!$conexao){
+die("Falha na conexão!" : mysql_connect_error());
+}
+mysql_close($sonexao); */
