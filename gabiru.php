0808d85d0924a3b1e9c64840a6198d48d73cb94d
Lucas Matheus <105503258+LmarDark@users.noreply.github.com>
2023-06-19 22:05:06 -0300

    Update gabiru.php

--- gabiru.php
+++ gabiru.php
@@ -1,17 +1,23 @@
+/*     VERSÃO 1.0 
+  $name = $_POST["name"];
+  $email =  $_POST["email"];
+  $password = $_POST["password"];
+  var_dump($name, $email, $password); */
+
 <?php 
 
-$name = $_POST["name"];
-$email =  $_POST["email"];
-$password = $_POST["password"];
-//var_dump($name, $email, $password);
-$servidor = localhost;
-$usuario = root;
-$senha = geralmente_nada/vazio;
-$db = não_me_recordo;
+$servidor = "XXXXXXX"; // Servidor do PhpMyAdM;
+$usuario = "XXXXXX"; // Usuário do seu PhpMyAdm;
+$senha = "XXXXXX"; // Senha do seu Usuário;
+$dbname = "XXXXXXXX"; // Nome do Banco de Dados;
 
-$conexao = mysql_connect($servidor, $usuario, $senha, $db);
+$conexao = mysqli_connect($servidor, $usuario, $senha, $dbname); // Conexão feita apartir da função "mysqli_connect";
+
+echo "<span style='color:green;'>Conexão Estabelecida!</span>";
 
 if(!$conexao){
-die("Falha na conexão!" : mysql_connect_error());
+die("Falha na conexão: " .mysqli_connect_error()); // Caso haja erro no servidor, retornar o nome do erro;
 }
-mysql_close($sonexao);
+
+mysqli_close($conexao);
+?>
