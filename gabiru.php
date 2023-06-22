a4254ce0f61b7f3cb9560d5ba0ba22a3f85397c4
Lucas Matheus <105503258+LmarDark@users.noreply.github.com>
2023-06-22 20:04:52 -0300

    Update gabiru.php

--- gabiru.php
+++ gabiru.php
@@ -19,27 +19,42 @@ mysqli_close($conexao);
 
         [VERSÃO 3.0]
 <?php 
+
 $name = $_POST["name"]; 
-$email = $_POST;["email"];
-$password = $_POST["password"];
-$servidor = "XXXXXXXX";
-$usuario = "XXXXXXXX";
-$senha = "XXXXXXXX";
-$dbname = "XXXXXXXXX";
+$email = $_POST["email"];
+$hash_pass = $_POST["password"];
+
+$hash = password_hash(($hash_pass), PASSWORD_DEFAULT);
+
+$servidor = "127.0.0.1";
+$usuario = "root";
+$senha = "";
+$dbname = "ninjaguerreirodb";
+
 $conexao = mysqli_connect($servidor, $usuario, $senha, $dbname);
+
 if(!$conexao){
 die("Falha na conexão: " .mysqli_connect_error());
 }
-$sql = "INSERT INTO ninjinha(Nome, Email, Senha)
+
+$sql = "INSERT INTO ninjinha(Nome, Email, Hashi)
         VALUES (?, ?, ?)";
+
 $stmt = mysqli_stmt_init($conexao);
+
 if ( ! mysqli_stmt_prepare($stmt, $sql)) {
+ 
     die(mysqli_error($conexao));
 }
-mysqli_stmt_bind_param($stmt, "ssi", $name, $email, $password);
+
+mysqli_stmt_bind_param($stmt, "sss", $name, $email, $hash);
+
 mysqli_stmt_execute($stmt);
+
 mysqli_close($conexao);
+
 echo "Cadastro Salvo!";
+
 ?>
 */
 
