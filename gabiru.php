759f15e6806b3e7e7117d11e53705de8425e6f96
Lucas Matheus <105503258+LmarDark@users.noreply.github.com>
2023-06-26 22:54:46 -0300

    Update gabiru.php

--- gabiru.php
+++ gabiru.php
@@ -1,22 +1,4 @@
 <?php 
-$name = $_POST["name"];
-$email = $_POST["email"];
-$hash_pass = $_POST["confirm_password"];
-
-print_r($_POST);
-
-$hash_pass = password_hash(($hash_pass), PASSWORD_DEFAULT);
-
-$servidor = "XXXXXX";
-$usuario = "XXXXXX";
-$senha = "XXXXXXX";
-$dbname = "XXXXXXXX";
-
-$conexao = mysqli_connect($servidor, $usuario, $senha, $dbname);
-
-if(!$conexao){
-die("Falha na conexão: " .mysqli_connect_error());
-}
 
 $sql = "INSERT INTO ninjinha(Nome, Email, Hashi) 
         VALUES ('$name', '$email', '$hash_pass')";
