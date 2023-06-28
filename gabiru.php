2ad423aa0ca9db5515f62c5cb34f3f276c7a9896
Lucas Matheus <105503258+LmarDark@users.noreply.github.com>
2023-06-28 20:39:08 -0300

    Update gabiru.php

--- gabiru.php
+++ gabiru.php
@@ -1,38 +1,12 @@
 <?php 
 
+require __DIR__ . "/database.php";
+
 $name = $_POST["name"];
 $email = $_POST["email"];
 $hash_pass = $_POST["confirm_password"];
 
-print_r($_POST);
-
 $hash_pass = password_hash(($hash_pass), PASSWORD_DEFAULT);
-
-$servidor = "XXXXXX";
-$usuario = "XXXXXXX";
-$senha = "XXXXXXX";
-$dbname = "XXXXXXXX";
-
-$conexao = mysqli_connect($servidor, $usuario, $senha, $dbname);
-
-if(!$conexao){
-die("Falha na conexão: " .mysqli_connect_error());
-}
-
-$sql = sprintf("SELECT * FROM user WHERE email = '%s'", $mysqli->real_escape_string($_POST["email"]));
-    
-    $result = $mysqli->query($sql);
-    
-    $user = $result->fetch_assoc();
-    
-    if ($user) {
-        
-        if (password_verify($_POST["$hash_pass"], $user["password_hash"])) {
-
-            die("Login successful");
-            }
-    }
-
 
 $sql = "INSERT INTO ninjinha(Nome, Email, Hashi) 
         VALUES ('$name', '$email', '$hash_pass')";
@@ -44,11 +18,13 @@ if ( ! mysqli_stmt_prepare($stmt, $sql)) {
     die(mysqli_error($conexao));
 }
 
-mysqli_stmt_bind_param($stmt, "sss", $name, $email, $hash);
+mysqli_stmt_bind_param($stmt, "sss", $name, $email, $hash_pass);
 
 mysqli_stmt_execute($stmt);
 
 mysqli_close($conexao);
+
+print_r($_POST);
 
 echo "Cadastro Salvo!";
 
