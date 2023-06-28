eb45c7b5b8519ee880b2acefd210080aa6a03232
Lucas Matheus <105503258+LmarDark@users.noreply.github.com>
2023-06-28 20:39:39 -0300

    Update database.php

--- database.php
+++ database.php
@@ -1,1 +1,14 @@
+<?php
 
+$servidor = "localhost";
+$usuario = "id20950902_ninjaguerreirocomdb";
+$senha = "Ninja01!";
+$dbname = "id20950902_ninjaguerreirosemdb";
+
+$conexao = mysqli_connect($servidor, $usuario, $senha, $dbname);
+
+if(!$conexao){
+die("Falha na conexão: " .mysqli_connect_error());
+}
+
+?>
