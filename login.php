cdbeacb9e76eafa902508b8db97574d1398f7b9a
Lucas Matheus <105503258+LmarDark@users.noreply.github.com>
2023-07-03 22:20:01 -0300

    Update login.php

--- login.php
+++ login.php
@@ -1,33 +1,24 @@
 <?php
 
 require __DIR__ . "/database.php";
-    
+include __DIR__ . "/index.php";
+
 $sql01 = sprintf("SELECT * FROM ninjinha WHERE Email = '%s'", $conexao->real_escape_string($_POST["email2"]));
     
     $result = $conexao->query($sql01);
     
     $ninjinha = $result->fetch_assoc();
-    
-    if ($nijinha) {
-        
+
+    if ($ninjinha) {
+
         if (password_verify($_POST["password2"], $ninjinha["Hashi"])) {
-            
-            session_start();
-            
-            session_regenerate_id();
             
             $_SESSION["user_id"] = $ninjinha["id"];
             
-            header("Location: index.html");
+            $is_available = false;
+            
             exit;
+            header("Location:index.php");
+            }
         }
-    }
-
 ?>
-<!DOCTYPE html>
-<html>
-<head>
-</head>
-<body>
-</body>
-</html>
