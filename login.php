eeb5cbb7fe0a5fb984b9bd5edb81b430ef363d97
Lucas Matheus <105503258+LmarDark@users.noreply.github.com>
2023-07-16 21:36:35 -0300

    Update login.php

--- login.php
+++ login.php
@@ -1,29 +1,41 @@
 <?php
 
-//        
-            $_SESSION["user_id"] = $ninjinha["id"];
-            
-            $is_available = false;
-
 require __DIR__ . "/database.php";
-include __DIR__ . "/index.php";
-
-$sql01 = sprintf("SELECT * FROM ninjinha WHERE Email = '%s'", $conexao->real_escape_string($_POST["email2"]));
+    
+    // include_once __DIR__ . "/index.php"; causador da mal renderização
+    
+    $sql01 = sprintf("SELECT * FROM ninjinha WHERE Email = '%s'", 
+    $conexao->real_escape_string($_POST["email2"]));
     
     $result = $conexao->query($sql01);
     
     $ninjinha = $result->fetch_assoc();
 
-    if ($ninjinha) {
-
+if ($ninjinha) {
+        
         if (password_verify($_POST["password2"], $ninjinha["Hashi"])) {
             
             $_SESSION["user_id"] = $ninjinha["id"];
             
-            $is_available = false;
+            include_once __DIR__ . "/index.php";
             
+            if ( ! isset($_SESSION["user_id"])) {
+            session_regenerate_id(true);
+            }
+            
+            ob_start();
+            $is_available = false;
+            $gabgame = (htmlspecialchars($ninjinha["Nome"]));
+            $trabay = ob_get_contents();
+            
+            header("Location: index.php");
+            echo $trabay;
+            ob_end_clean();
             exit;
-            header("Location:index.php");
+            
             }
         }
+    
 ?>
+    
+  
