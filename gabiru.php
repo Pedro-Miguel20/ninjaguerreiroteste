f002877263254177957b7d3f34f040b10901f63b
Lucas Matheus <105503258+LmarDark@users.noreply.github.com>
2023-07-16 21:36:03 -0300

    Update gabiru.php

--- gabiru.php
+++ gabiru.php
@@ -1,14 +1,17 @@
 <?php 
 
+session_start();
+
 require __DIR__ . "/database.php";
 
 $name = $_POST["name"];
 $email = $_POST["email"];
 $hash_pass = $_POST["confirm_password"];
+$bio = "Escreva sobre você aqui...";
 
 $hash_pass = password_hash(($hash_pass), PASSWORD_DEFAULT);
 
-$sql = "INSERT INTO ninjinha(Nome, Email, Hashi) VALUES (?,?,?)";
+$sql = "INSERT INTO ninjinha(Nome, Email, Hashi, bio) VALUES (?,?,?,?)";
 
 $stmt = mysqli_stmt_init($conexao);
 
@@ -17,16 +20,15 @@ if ( ! mysqli_stmt_prepare($stmt, $sql)) {
     die(mysqli_error($conexao));
 }
 
-mysqli_stmt_bind_param($stmt, "sss", $name, $email, $hash_pass);
+mysqli_stmt_bind_param($stmt, "ssss", $name, $email, $hash_pass, $bio);
 
 mysqli_stmt_execute($stmt);
 
 mysqli_close($conexao);
 
-print_r($_POST);
+session_regenerate_id();
 
-echo "Cadastro Salvo!";
+unset($bio);
 
-
-
-?>
+header("Location: index.php");
+exit;
