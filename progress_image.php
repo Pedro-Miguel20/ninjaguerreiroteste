fb66b6c970f00b630cebde2da224fe02325c5cb8
Lucas Matheus <105503258+LmarDark@users.noreply.github.com>
2023-08-23 23:31:11 -0300

    Update progress_image.php

--- progress_image.php
+++ progress_image.php
@@ -1,9 +1,34 @@
 <?php
-session_set_cookie_params(['httponly' => true]);
+require __DIR__ . "/index.php";
 session_start();
 
-session_destroy();
+$salsicha = $_SESSION['user_id'];
+$opaitaon = $_POST["opaitaon"];
+$_SESSION["link_db"] = $opaitaon;
 
-header("Location: index.php");
+require __DIR__ . "/database.php";
 
+$sql_image = ("UPDATE ninjinha
+    SET img = ?
+    WHERE id = ?
+    ");
+
+$stmt = mysqli_stmt_init($conexao);
+
+if ( ! mysqli_stmt_prepare($stmt, $sql_image)) {
+ 
+    die(mysqli_error($conexao));
+}
+
+mysqli_stmt_bind_param($stmt, "si", $opaitaon, $salsicha);
+
+mysqli_stmt_execute($stmt);
+
+mysqli_close($conexao);
+
+header("refresh: 0, url = index.php");
+
+$update = true;
+ 
+// Sem ele, ele envia informações infinitas
 ?>
