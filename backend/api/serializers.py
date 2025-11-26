f8159755fd81ce019a6993897346bb15a97f4d75
LmarDark <lucasmatheusalero@gmail.com>
2025-11-26 10:48:18 -0400

    refactor: organizando diretórios

---
+++ backend/api/serializers.py
@@ -0,0 +1,20 @@
+from django.contrib.auth.models import User
+from rest_framework import serializers
+
+class UserCreateSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = User
+        fields = ['username', 'password']
+
+    def create(self, validated_data):
+        user = User.objects.create_user(
+            username=validated_data['username'],
+            password=validated_data['password']
+        )
+        return user
+
+class LoginSerializer(serializers.Serializer):
+    username = serializers.CharField()
+    password = serializers.CharField()
