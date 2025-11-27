b2aef6da482dc9111d28ad6b2d0d58f97c0cc01d
LmarDark <lucasmatheusalero@gmail.com>
2025-11-27 11:13:39 -0400

    feat: adicionado rotas v1/token e v1/token/refresh para funcionamento do JWT

--- backend/ninjaguerreiro/urls.py
+++ backend/ninjaguerreiro/urls.py
@@ -15,8 +15,21 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from rest_framework import routers
+from django.urls import include, path
+#from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from api.views import CustomTokenObtainPairView, CustomTokenRefreshView
+
+from api import views
+
+router = routers.DefaultRouter()
+#router.register(r'users', views.UserViewSet)
+#router.register(r'groups', views.GroupViewSet)
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('v1/token/', CustomTokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('v1/token/refresh/', CustomTokenRefreshView.as_view(), name='token_refresh'),
+    #path('', include(router.urls)),
+    #path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    #path('admin/', admin.site.urls),
 ]
