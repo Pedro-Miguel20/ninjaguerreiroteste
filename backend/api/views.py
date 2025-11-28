89864de52cf64d593ef63693b10de20bfd098b87
LmarDark <lucasmatheusalero@gmail.com>
2025-11-28 11:49:16 -0400

    feat: criado ViewSets no view.py

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,4 +1,15 @@
-#from django.contrib.auth import authenticate
-#from django.contrib.auth.models import User
-#from rest_framework.permissions import AllowAny
+from django.contrib.auth.models import User, Group
+from rest_framework import permissions, viewsets
 
+from api.serializers import UserSerializer, GroupSerializer
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all().order_by("-date_joined")
+    serializer_class = UserSerializer
+    permission_classes = [permissions.IsAuthenticated]
+    
+class GroupViewSet(viewsets.ModelViewSet):
+    queryset = Group.objects.all().order_by("name")
+    serializer_class = GroupSerializer
+    permission_classes = [permissions.IsAuthenticated]
