7df66a87c7ebd7cbd6df543f2d004346e9287636
LmarDark <lucasmatheusalero@gmail.com>
2025-12-03 08:49:46 -0400

    refactor: adicionando host do backend

--- backend/ninjaguerreiro/settings.py
+++ backend/ninjaguerreiro/settings.py
@@ -61,7 +61,9 @@ CORS_ALLOWED_ORIGINS = env.list(
     "CORS_ALLOWED_ORIGINS",
     default=[
         "http://localhost:3000",
-        "http://127.0.0.1:3000"
+        "http://127.0.0.1:3000",
+        "http://127.0.0.1:8000",
+        "http://localhost:8000"
     ],
 )
 
