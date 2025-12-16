a4dbf6f4e6efa40982ba776987012943e9a7723d
Pedro-Miguel20 <pm987162@gmail.com>
2025-12-16 08:01:06 -0400

    Minor fix: literally one digit

--- frontend/src/utils/passwordSchema.ts
+++ frontend/src/utils/passwordSchema.ts
@@ -6,7 +6,7 @@ passwordSchema
   .is().min(8)                 // mínimo 8 caracteres
   .is().max(100)               // máximo 100 caracteres
   .has().uppercase()           // pelo menos 1 letra maiúscula
-  .has().digits(2)             // pelo menos 2 números
+  .has().digits(1)             // pelo menos 1 números
   .has().not().spaces()        // não pode conter espaços
   .is().not().oneOf([
     "Passw0rd",
