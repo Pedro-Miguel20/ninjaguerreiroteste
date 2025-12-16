39dbc6faf1cb7813a763d9b744afae5354f93ff6
Pedro-Miguel20 <pm987162@gmail.com>
2025-12-16 10:04:15 -0400

    Feature: initializing new route for landing page

--- frontend/src/app/[lang]/page.tsx
+++ frontend/src/app/[lang]/page.tsx
@@ -1,18 +1,11 @@
-import { getDictionaryServerOnly } from "@/src/dictionaries/default-dictionary-server-only";
+import { redirect } from 'next/navigation'
 import { Locale } from "@/src/config/i18n.config";
 
-export default async function Home({
+export default async function Page({
   params,
 }: {
   params: Promise<{ lang: Locale }>;
 }) {
   const { lang } = await params; // unwrap the promise
-  const dict = await getDictionaryServerOnly(lang);
-
-  return (
-    <main>
-      <h1>{dict.site.name}</h1>
-      <p>{dict.site.description}</p>
-    </main>
-  );
+  redirect(`/${lang}/welcome`)
 }
---
+++ frontend/src/app/[lang]/welcome/page.tsx
@@ -0,0 +1,18 @@
+import { getDictionaryServerOnly } from "@/src/dictionaries/default-dictionary-server-only";
+import { Locale } from "@/src/config/i18n.config";
+
+export default async function welcome({
+  params,
+}: {
+  params: Promise<{ lang: Locale }>;
+}) {
+  const { lang } = await params; // unwrap the promise
+  const dict = await getDictionaryServerOnly(lang);
+
+  return (
+    <main>
+      <h1>{dict.site.name}</h1>
+      <p>{dict.site.description}</p>
+    </main>
+  );
+}
