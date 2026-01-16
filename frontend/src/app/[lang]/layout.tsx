335582aca8569be59c15af294db5474e30add4af
Pedro-Miguel20 <pm987162@gmail.com>
2026-01-16 12:14:17 -0400

    Overall correction: header, routes, translations, error responses

--- frontend/src/app/[lang]/layout.tsx
+++ frontend/src/app/[lang]/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "../../app/globals.css";
 import { Providers } from "./providers";
-import { i18n, Locale } from "@/src/config/i18n.config";
+import { i18n } from "@/src/config/i18n.config";
 import { ReactNode } from "react";
 import { getDictionaryServerOnly } from "@/src/dictionaries/default-dictionary-server-only";
 
